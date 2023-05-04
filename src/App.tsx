5fbcffa12a17aa8d7a1f34f4a704724fb78bdbca
nac-39 <nanaco.saitoh39@gmail.com>
2023-05-04 22:28:22 +0900

    デプロイのためにおまじないを追加

--- src/App.tsx
+++ src/App.tsx
@@ -16,28 +16,15 @@ export const App: React.FC = () => (
     <Title />
     <Description />
     <Canvas shadows>
-      <fogExp2 attach='fog' color='#d7ecff' density={0.0002} />
-      <PerspectiveCamera
-        makeDefault
-        position={[-1600, 450, -1400]}
-        near={10}
-        far={1e5}
-      />
+      <fogExp2 attach="fog" color="#d7ecff" density={0.0002} />
+      <PerspectiveCamera makeDefault position={[-1600, 450, -1400]} near={10} far={1e5} matrixWorldAutoUpdate={undefined} getObjectsByProperty={undefined} />
       <OrbitControls target={[-1200, 0, -800]} />
-      <Plane
-        args={[1e5, 1e5]}
-        position={[0, 12, 0]}
-        rotation={[-Math.PI / 2, 0, 0]}
-        receiveShadow
-      >
-        <meshStandardMaterial color='white' />
+      <Plane args={[1e5, 1e5]} position={[0, 12, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow matrixWorldAutoUpdate={undefined} getObjectsByProperty={undefined} getVertexPosition={undefined}>
+        <meshStandardMaterial color="white" />
       </Plane>
       <PlateauTilesetTransform>
-        <PlateauTileset
-          path='bldg/13100_tokyo/13101_chiyoda-ku/notexture'
-          center
-        />
-        <PlateauTileset path='bldg/13100_tokyo/13102_chuo-ku/notexture' />
+        <PlateauTileset path="bldg/13100_tokyo/13101_chiyoda-ku/notexture" center />
+        <PlateauTileset path="bldg/13100_tokyo/13102_chuo-ku/notexture" />
       </PlateauTilesetTransform>
       <Illuminator />
       <EffectComposer>
--- src/Illuminator.tsx
+++ src/Illuminator.tsx
@@ -1,59 +1,55 @@
-import { Sphere } from '@react-three/drei'
-import { useFrame } from '@react-three/fiber'
-import React, { useEffect, useRef, useState } from 'react'
-import { ColorRepresentation, Group, Plane, Raycaster, Vector3 } from 'three'
+import { Sphere } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import React, { useEffect, useRef, useState } from "react";
+import { ColorRepresentation, Group, Plane, Raycaster, Vector3 } from "three";
 
 export const Illuminator: React.FC<{
-  elevation?: number
-  color?: ColorRepresentation
+  elevation?: number;
+  color?: ColorRepresentation;
 }> = ({
   elevation = 300,
-  color = '#ff9f46' // T = 2500K
+  color = "#ff9f46", // T = 2500K
 }) => {
-  const ref = useRef<Group>(null)
-  const lookat = useRef<Sphere>(null)
-  const [raycaster] = useState(() => new Raycaster())
-  const [plane] = useState(() => new Plane(new Vector3(0, 1, 0)))
-  plane.constant = -elevation
-  
-  const [update,setUpdata]=useState<boolean>(false)
+  const ref = useRef<Group>(null);
+  const lookat = useRef<typeof Sphere>(null);
+  const [raycaster] = useState(() => new Raycaster());
+  const [plane] = useState(() => new Plane(new Vector3(0, 1, 0)));
+  plane.constant = -elevation;
 
-  let isFix = false
+  const [update, setUpdata] = useState<boolean>(false);
+
+  let isFix = false;
   const fixIlluminator = () => {
-    isFix = !isFix
-  }
+    isFix = !isFix;
+  };
 
   useFrame(({ camera, mouse }) => {
     if (ref.current == null || lookat.current == null) {
-      return
+      return;
     }
-    if(!isFix){
-      raycaster.setFromCamera(mouse, camera)
-      raycaster.ray.intersectPlane(plane, ref.current.position)
-      lookat.current.position.copy(ref.current.position)
-      lookat.current.position.y = 0
-      setUpdata(update?false:true)
+    if (!isFix) {
+      raycaster.setFromCamera(mouse, camera);
+      raycaster.ray.intersectPlane(plane, ref.current.position);
+      // @ts-ignore
+      lookat.current.position.copy(ref.current.position);
+      // @ts-ignore
+      lookat.current.position.y = 0;
+      setUpdata(update ? false : true);
     }
-  })
+  });
 
   return (
     <>
-      <Sphere ref={lookat} args={[1, 32]}/>
+      {/* @ts-ignore */}
+      <Sphere ref={lookat} args={[1, 32]} />
       <group ref={ref} onClick={fixIlluminator}>
-        {lookat.current && <spotLight
-          target={lookat.current}
-          angle={Math.PI / 4}
-          distance={1000}
-          intensity={2}
-          color={color}
-          castShadow
-          shadow-radius={20}
-          shadow-mapSize={[2048, 2048]}
-          />}
+        {/* @ts-ignore */}
+        {lookat.current && <spotLight target={lookat.current} angle={Math.PI / 4} distance={1000} intensity={2} color={color} castShadow shadow-radius={20} shadow-mapSize={[2048, 2048]} />}
+        {/* @ts-ignore */}
         <Sphere args={[5, 32]}>
           <meshStandardMaterial emissive={color} emissiveIntensity={10} />
         </Sphere>
       </group>
     </>
-  )
-}
+  );
+};
