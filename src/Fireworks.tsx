f4f19c04ae5a2a48a937c20e13e584427ef585a6
Lucas544875 <takayanagi691@gmail.com>
2023-05-04 16:12:49 +0000

    update 花火の作成を関数に切り出し

--- src/Fireworks.tsx
+++ src/Fireworks.tsx
@@ -11,38 +11,44 @@ import {
 
 import type { Mesh } from 'three';
 
+
+
+function makeFireworks(radious, length){
+  // 頂点情報を格納する配列
+  const vertices = [];
+  for (let i = 0; i < length; i++) {
+    const r = radious;
+    const theta = 2 * Math.PI * Math.random();
+    const phi = Math.PI * Math.random();
+    const x = r * Math.sin(theta) * Math.cos(phi);
+    const y =  r * Math.sin(theta) * Math.sin(phi);
+    const z = r * Math.cos(theta);
+    
+    vertices.push(x, y, z);
+  }
+  
+  // 形状データを作成
+  const geometry = new BufferGeometry();
+  geometry.setAttribute('position', new Float32BufferAttribute(vertices, 3));
+  
+  // マテリアルを作成
+  const material = new PointsMaterial({
+    // 一つ一つのサイズ
+    size: 10,
+    // 色
+    color: "#CB4829"
+  });
+  
+  // 物体を作成
+  const mesh = new Points(geometry, material);
+  return mesh
+}
+
 // 形状データを作成
 const SIZE = 100;
 // 配置する個数
 const LENGTH = 1000;
-// 頂点情報を格納する配列
-const vertices = [];
-for (let i = 0; i < LENGTH; i++) {
-  const r = SIZE;
-  const theta = 2 * Math.PI * Math.random();
-  const phi = Math.PI * Math.random();
-  const x = r * Math.sin(theta) * Math.cos(phi);
-  const y =  r * Math.sin(theta) * Math.sin(phi);
-  const z = r * Math.cos(theta);
-
-  vertices.push(x, y, z);
-}
-
-// 形状データを作成
-const geometry = new BufferGeometry();
-geometry.setAttribute('position', new Float32BufferAttribute(vertices, 3));
-
-// マテリアルを作成
-const material = new PointsMaterial({
-  // 一つ一つのサイズ
-  size: 10,
-  // 色
-  color: "#CB4829"
-});
-
-// 物体を作成
-const mesh = new Points(geometry, material);
-
+const mesh = makeFireworks(SIZE, LENGTH)
 
 export interface FireworksProps {
   position: Vector3
