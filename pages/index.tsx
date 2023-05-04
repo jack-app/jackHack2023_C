17388dff708617bead186ab776a0766986ea53fd
nac-39 <nanaco.saitoh39@gmail.com>
2023-05-04 22:51:32 +0900

    add meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,30 @@
-import { Global, css } from '@emotion/react'
-import { NextPage } from 'next'
-import React from 'react'
+import { Global, css } from "@emotion/react";
+import { NextPage } from "next";
+import React from "react";
+import Head from "next/head";
 
-import { App } from '../src/App'
+import { App } from "../src/App";
 
 const Index: NextPage = () => {
+  const title = "打ち上げ花火、下調べしてから見るか";
+  const description = "打ち上げ花火が見える場所を下調べできるwebアプリです";
+  const url = "https://jack-hack2023-c.vercel.app";
+  const imgUrl = "https://jack-hack2023-c.vercel.app/images/title_5.png";
   return (
     <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content={description} />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:site_name" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={imgUrl} />
+        <meta property="og:image:width" content="2940" />
+        <meta property="og:image:height" content="1654" />
+        <meta name="twitter:card" content="summary" />
+      </Head>
       <Global
         styles={css`
           html,
@@ -20,7 +38,7 @@ const Index: NextPage = () => {
       />
       <App />
     </>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
