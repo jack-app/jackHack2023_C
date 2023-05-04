77643137b5f74f5dbcfb52fa1392c9ed8ba97554
ai8nik <ai8nik@gmail.com>
2023-05-04 18:27:11 +0900

    Title変更２

--- src/Title.tsx
+++ src/Title.tsx
@@ -5,24 +5,9 @@ export const Title: React.FC = () => (
   // Reactでは、複数の要素を返すことができない。
   // そのため、以下のようにReact.Fragmentを使う必要がある
   <>
-    <h1
-      css={css`
-        color: black;
-        font-weight: bold;
-        // background: black;
-        writing-mode: vertical-rl;
-        // border: 5px solid black;
-      `}
-    >
-      {/* 打ち上げ花火、下調べしてから見るか */}
-      {/* <img src="/images/618018_l 2.jpg"width = {1450} height = {600} /> */}
-    </h1>
-    {/* import Image from 'next/image'
-
-    export const Avatar: React.FC = () => (
-    <Image src="618018_l 2.jpg" width={64} height={64} alt="My avatar" />
-    ) */}
-    {/* <img src="/images/618018_l 2.jpg"width = {1450} height = {600} /> */}
-    <img src="/images/title.png"width = {1450} height = {600} />
+    
+    
+    <img src="/images/title_5.png"width = {1450} height = {850}/>
+    {/* <img src="/images/title.png"width = {1450} height = {1100}/> */}
   </>
 );
