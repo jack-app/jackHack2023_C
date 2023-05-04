a3be27ff9cb39b4497846a22dcc05ee5bb10eecf
nac-39 <nanaco.saitoh39@gmail.com>
2023-05-05 03:29:20 +0900

    wip

--- src/LocationSelect.tsx
+++ src/LocationSelect.tsx
@@ -3,11 +3,11 @@ import { css } from "@emotion/react";
 import { LocationPaths, FireWorks } from "./constants";
 
 interface Props {
-  onChangeLocation: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  onChangeLocation: (locationUrl: string, location: { x: number; y: number; z: number }) => void;
 }
 export const RawLocationSelect: React.FC<Props> = ({ onChangeLocation }) => (
   <>
-    <select onChange={onChangeLocation}>
+    <select >
       {LocationPaths.map((path) => {
         if (path.TYPE == "bldg" && path.URL.match(/notexture/))
           return (
@@ -21,47 +21,35 @@ export const RawLocationSelect: React.FC<Props> = ({ onChangeLocation }) => (
 );
 
 export const LocationSelect: React.FC<Props> = ({ onChangeLocation }) => (
-  <>
-    <label
-      css={css`
-        display: inline-flex;
-        align-items: center;
-        position: relative;
-
-        &::after {
-          position: absolute;
-          right: 15px;
-          width: 10px;
-          height: 7px;
-          background-color: #535353;
-          clip-path: polygon(0 0, 100% 0, 50% 100%);
-          content: "";
-          pointer-events: none;
-        }
-
-        select {
-          appearance: none;
-          min-width: 230px;
-          height: 2.8em;
-          padding: 0.4em calc(0.8em + 30px) 0.4em 0.8em;
-          border: 1px solid #cccccc;
-          border-radius: 3px;
-          background-color: #fff;
-          color: #333333;
-          font-size: 1em;
+  <div>
+    {FireWorks.map((firework) => {
+      return (
+        <div 
+        tabIndex={0}
+        onKeyDown={(e) => {onChangeLocation(firework.tilesetUrl, firework.location)}}
+        onClick={(e) => {onChangeLocation(firework.tilesetUrl, firework.location)}}
+        role="button"
+        css={css`
+          border: 1px solid #446173;
+          margin: 0 0 -1px;
+          &:hover {
+            background-color: #446173;
+            color: #fff;
+          }
           cursor: pointer;
-        }
-      `}
-    >
-      <select onChange={onChangeLocation}>
-        {FireWorks.map((path) => {
-          return (
-            <option key={path.name} value={[path.tilesetUrl, path.location.x.toString(), path.location.y.toString(), path.location.z.toString()].join(",")}>
-              {path.name}, {path.place}
-            </option>
-          );
-        })}
-      </select>
-    </label>
-  </>
+          padding: 0.2em;
+        `}>
+          <div css={css`
+            font-weight: bold;
+          `}
+          >{firework.name}</div>
+          <span css={css`
+            font-size: 0.8em;
+            color: #446173;
+          `}
+          >{firework.place}</span>
+        </div>
+      );
+    })}
+  </div>
 );
